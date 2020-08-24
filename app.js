357b1de161111059386eb703fc80a73e3166acf7
omarlv86 <40244889+omarlv86@users.noreply.github.com>
2020-08-24 16:42:08 -0500

    Creando objetos Product y metodos

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-alert('Prueba')
+class Product {
+    constructor(name, price, year){
+        this.name = name;
+        this.price = price;
+        this.year = year;
+    }    
+}
+
+class UI {
+    addProduct(){
+
+    }
+
+    deleteProduct(){
+
+    }
+
+    showMessage(){
+
+    }
+}
+
+
+// DOM Events
+document.getElementById('product-form').addEventListener('submit', function(e){
+    const name = document.getElementById('name').value;
+    const price = document.getElementById('price').value;
+    const year = document.getElementById('year').value;
+
+    console.log(name, price, year);
+
+    console.log(new Product(name, price, year))
+
+    e.preventDefault();
+
+})
